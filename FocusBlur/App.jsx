22b95270b27e527c40820bc2e39e5ab4ad9e9250
Rahulgauthamodela <89241127+Methedexter@users.noreply.github.com>
2025-04-23 10:43:07 +0530

    Add files via upload

---
+++ FocusBlur/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import React from "react";
+// import TextUpdater from './TextUpdater';
+import FocusBlurDemo from "../components/FocusBlur";
+
+function App() {
+  return (
+    <>
+      {/* <div>
+        <TextUpdater />
+      </div> */}
+
+      <div>
+        <FocusBlurDemo />
+      </div>
+    </>
+  );
+}
+export default App;
