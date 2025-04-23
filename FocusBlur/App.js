4a842975dbaec8236c05a96a285007e654a79158
Rahulgauthamodela <89241127+Methedexter@users.noreply.github.com>
2025-04-23 10:08:45 +0530

    Add files via upload
    FocusBlur

---
+++ FocusBlur/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import FocusBlurDemo from './FocusBlurDemo';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <FocusBlurDemo />
+    </div>
+  );
+}
+
+export default App;
