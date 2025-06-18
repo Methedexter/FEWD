b2a3cc5474af1a2b141278e5d822354685e2a328
Methedexter <rgautham45@gmail.com>
2025-06-18 10:55:18 +0530

    FEWD Assignment EXP9

---
+++ src/About/About.jsx
@@ -0,0 +1,14 @@
+import AboutChild1 from './AboutChild1';
+import AboutChild2 from './AboutChild2';
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <AboutChild1 />
+      <AboutChild2 />
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Home/HomeChild1.jsx
@@ -0,0 +1,5 @@
+function HomeChild1() {
+  return <p>This is Home Child 1 content.</p>;
+}
+
+export default HomeChild1;
